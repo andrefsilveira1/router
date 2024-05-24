9aba71148b40d462f016c0bb051575eef3e116c7
André Freitas <freitasandre38@gmail.com>
2024-05-23 21:27:35 -0300

    feat: create aggregates functions

--- algorithm/domain/aggregates.go
+++ algorithm/domain/aggregates.go
@@ -3,3 +3,20 @@ package domain
 type Graph struct {
 	nodes map[int]*Node
 }
+
+func CreateGraph() *Graph {
+	return &Graph{
+		nodes: make(map[int]*Node),
+	}
+}
+
+func (g *Graph) AddNode(node *Node) {
+	if _, existent := g.nodes[node.Id]; !existent {
+		g.nodes[node.Id] = node
+	}
+}
+
+func (g *Graph) GetNode(id int) (*Node, bool) {
+	node, exists := g.nodes[id]
+	return node, exists
+}
