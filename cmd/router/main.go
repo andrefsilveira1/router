322382526525ad086f46a71d0a3a3e546517ca7f
André Freitas <freitasandre38@gmail.com>
2024-05-24 18:02:19 -0300

    feat: create dijkstra algorithm with cmd router

---
+++ cmd/router/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	services "andrefsilveira1/router/internal/application/service"
+	"andrefsilveira1/router/internal/domain/entities"
+	"fmt"
+)
+
+func main() {
+	// Instanciate graph and nodes
+	graph := entities.CreateGraph()
+
+	node1 := entities.CreateNode(1)
+	node2 := entities.CreateNode(2)
+	node3 := entities.CreateNode(3)
+	node4 := entities.CreateNode(4)
+
+	node1.AppendAdjacent(2, 1)
+	node1.AppendAdjacent(3, 4)
+	node2.AppendAdjacent(3, 2)
+	node2.AppendAdjacent(4, 7)
+	node3.AppendAdjacent(4, 3)
+
+	graph.AddNode(node1)
+	graph.AddNode(node2)
+	graph.AddNode(node3)
+	graph.AddNode(node4)
+
+	// Applying dijkstra algorithm
+
+	distances := services.Dijkstra(graph, 1)
+
+	// I really think that we should display this in a different way...
+	// Maybe create a figma example using React ?
+
+	fmt.Println("Shortest distances from node 1:")
+	for node, distance := range distances {
+		fmt.Printf("Node %d: %d\n", node, distance)
+	}
+
+}
---
+++ internal/application/service/graph_service.go
@@ -0,0 +1,83 @@
+package services
+
+import (
+	"andrefsilveira1/router/internal/domain/entities"
+	"container/heap"
+	"math"
+)
+
+type PriorityQueue []*Item
+
+type Item struct {
+	node     *entities.Node
+	priority int
+	index    int
+}
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority < pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, node *entities.Node, priority int) {
+	item.node = node
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func Dijkstra(graph *entities.Graph, startId int) map[int]int {
+	dist := make(map[int]int)
+	for id := range graph.Nodes {
+		dist[id] = math.MaxInt32
+	}
+	dist[startId] = 0
+
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+	heap.Push(pq, &Item{
+		node:     graph.Nodes[startId],
+		priority: 0,
+	})
+
+	for pq.Len() > 0 {
+		item := heap.Pop(pq).(*Item)
+		currentNode := item.node
+		currentDistance := dist[currentNode.Id]
+
+		for adjacentId, weight := range currentNode.Adjacent {
+			alt := currentDistance + weight
+			if alt < dist[adjacentId] {
+				dist[adjacentId] = alt
+				heap.Push(pq, &Item{
+					node:     graph.Nodes[adjacentId],
+					priority: alt,
+				})
+			}
+		}
+	}
+
+	return dist
+}
