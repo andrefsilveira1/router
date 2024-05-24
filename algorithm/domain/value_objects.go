9a8fb899d190ae14ba9be97880438a7134982f08
André Freitas <freitasandre38@gmail.com>
2024-05-23 21:24:12 -0300

    feat: create value objects functions to node

--- algorithm/domain/value_objects.go
+++ algorithm/domain/value_objects.go
@@ -4,3 +4,14 @@ type Node struct {
 	Id       int
 	Adjacent map[int]int
 }
+
+func Append_node(id int) *Node {
+	return &Node{
+		Id:       id,
+		Adjacent: make(map[int]int),
+	}
+}
+
+func (n *Node) Append_adjacent(adjancentId, weight int) {
+	n.Adjacent[adjancentId] = weight
+}
