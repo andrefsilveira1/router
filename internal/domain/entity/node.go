81092b1b833e398174a4015656ad503294cacd08
André Freitas <freitasandre38@gmail.com>
2024-06-11 20:45:17 -0300

    feat: create entity level functions

--- internal/domain/entity/node.go
+++ internal/domain/entity/node.go
@@ -1,5 +1,7 @@
 package entity
 
+import "fmt"
+
 type Node struct {
 	Id       int
 	X        int
@@ -20,4 +22,15 @@ func CreateNode(id, x, y int) *Node {
 
 func (n *Node) AppendAdjacent(adjacentId, weight int) {
 	n.Adjacent[adjacentId] = weight
+}
+
+func (n *Node) SendPackage(pack map[string]interface{}) {
+	n.Package = pack
+}
+
+func (n *Node) PrintPackageInfo() {
+	fmt.Printf("Package info for Node %d:\n", n.Id)
+	for key, value := range n.Package {
+		fmt.Printf("  %s: %v\n", key, value)
+	}
 }
