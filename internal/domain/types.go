73cad0885d5103b3e821c9bca7ceed030c2df4a2
André Freitas <freitasandre38@gmail.com>
2024-07-01 20:38:16 -0300

    feat: create value objects methods

--- internal/domain/types.go
+++ internal/domain/types.go
@@ -13,6 +13,13 @@ type Mesh struct {
 	Size  int
 }
 
+type Queue struct {
+	node     *Node
+	priority int
+	index    int
+}
+type PriorityQueue []*Queue
+
 func (g *Mesh) Disable(x, y int) {
 	id := y*g.Size + x
 	if node, exists := g.Nodes[id]; exists {
@@ -25,4 +32,32 @@ func (g *Mesh) Enable(x, y int) {
 	if node, exists := g.Nodes[id]; exists {
 		node.Blocked = false
 	}
+}
+
+func (q PriorityQueue) Len() int { return len(q) }
+
+func (q PriorityQueue) Less(i, j int) bool {
+	return q[i].priority < q[j].priority
+}
+
+func (q PriorityQueue) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
+	q[j].index = j
+}
+
+func (q *PriorityQueue) Push(x interface{}) {
+	n := len(*q)
+	queue := x.(*Queue)
+	queue.index = n
+	*q = append(*q, queue)
+}
+
+func (q *PriorityQueue) Pop() interface{} {
+	old := *q
+	n := len(old)
+	queue := old[n-1]
+	queue.index = -1
+	*q = old[0 : n-1]
+	return queue
 }
