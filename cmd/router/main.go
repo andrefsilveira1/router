496a292a40f3305a57ccf30f9358b7a5ed0c55bf
André Freitas <freitasandre38@gmail.com>
2024-07-01 20:57:06 -0300

    feat: instance algorithm inside main.go

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -9,6 +9,8 @@ func main() {
 	mesh := service.CreateMesh()
 	mesh.Disable(3, 3)
 	mesh.Disable(4, 4)
+	mesh.Disable(2, 5)
+	mesh.Disable(5, 2)
 
 	startNode, _ := mesh.GetNode(0, 0)
 	goalNode, _ := mesh.GetNode(7, 7)
@@ -18,8 +20,6 @@ func main() {
 		fmt.Println("No path found.")
 	} else {
 		fmt.Printf("Path found with %d hops:\n", hops)
-		for _, node := range path {
-			fmt.Printf("Node %d (%d, %d)\n", node.Id, node.X, node.Y)
-		}
+		mesh.Print(startNode, goalNode, path)
 	}
 }
